30f49474362267964a24b1b7fee2caac5e2fb83b
PawankumarPK <pk9910765616@gmail.com>
2019-11-13 21:50:54 +0530

    update AI listening in welcom fragment

--- app/src/main/java/ai/jetbrain/arya/fragment/BaseFragment.kt
+++ app/src/main/java/ai/jetbrain/arya/fragment/BaseFragment.kt
@@ -1,66 +1,18 @@
 package ai.jetbrain.arya.fragment
 
-import ai.api.AIConfiguration
-import ai.api.AIListener
-import ai.api.android.AIService
-import ai.api.model.AIError
-import ai.api.model.AIResponse
-import ai.jetbrain.arya.R
-import ai.jetbrain.arya.SttUtils.ConversionCallback
 import ai.jetbrain.arya.SttUtils.Speech
-import ai.jetbrain.arya.SttUtils.TranslatorFactory
 import ai.jetbrain.arya.activity.BaseActivity
-import ai.jetbrain.arya.utils.CheckNetwork
-import ai.jetbrain.arya.utils.Helper
-import ai.jetbrain.arya.utils.okhttpInterceptor
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.os.Handler
 import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
-import com.google.gson.GsonBuilder
-import okhttp3.OkHttpClient
-import org.json.JSONArray
-import org.json.JSONObject
 
 
 open class BaseFragment : Fragment(){
 
     protected lateinit var baseActivity: BaseActivity
 
-    private var currentText = ""
-
-    private var isBusy = false
-
     protected var speechEnabled = true
-
-   /* private val c = object : ConversionCallback {
-        override fun onSuccess(result: String) {
-            Log.i("HEARD", result)
-            //matchSpeech(result)
-            updateText(result)
-            //baseActivity.muteSound(false)
-           // dialogFlow(result)
-            textRetained()
-
-
-        }
-
-        override fun onCompletion() {
-
-        }
-
-        override fun onErrorOccurred(errorMessage: String) {
-            Log.e("ERROR", errorMessage)
-            //isBusy = false
-            updateText("Sorry, I didn't get that.")
-            Speech.speakOutText("Sorry, I didn't get that.")
-            //welcome()
-        }
-
-    }
-*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -69,9 +21,7 @@ open class BaseFragment : Fragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         initSpeechAndText()
-
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
@@ -80,23 +30,16 @@ open class BaseFragment : Fragment(){
     }
 
     private fun initSpeechAndText() {
-        Speech.initSTT(baseActivity)
         Speech.initTTS(baseActivity)
-        Speech.setOnCompleteListener {
-            isBusy = false
-        }
     }
 
-    protected fun listen() {
+    /*protected fun listen() {
         if (!isBusy) {
             isBusy = true
             updateText("Listening...")
-           // baseActivity.muteSound(true)
-           /* TranslatorFactory.with(TranslatorFactory.TRANSLATORS.SPEECH_TO_TEXT, c)
-                .initialize(baseActivity)*/
         }
-    }
-
+    }*/
+    /*
     private fun welcome() {
         val welcome_start_text = resources.getString(R.string.welcome_start_text)
         val welcome_text_array = resources.getStringArray(R.array.welcome_text)
@@ -104,7 +47,8 @@ open class BaseFragment : Fragment(){
                 welcome_text_array[Math.floor(Math.random() * welcome_text_array.size).toInt()]
         Speech.speakOutText(speak_text)
     }
-
+    */
+    /*
     private fun matchSpeech(speech: String) {
 
         val queries = resources.getStringArray(R.array.queries)
@@ -120,77 +64,14 @@ open class BaseFragment : Fragment(){
         updateText(talkBackText)
         Speech.speakOutText(talkBackText)
     }
+*/
 
     open fun onFaceUpdate(facex: Float, facey: Float) {
         if (facex > 0 && facex < 1 && facey > 0 && facey < 1) {
             Log.i("Face", "$facex,$facey")
-            if (speechEnabled) listen()
         }
     }
-
-    open fun updateText(txt: String) {
-        currentText = txt
-    }
-
-
 }
-
-
-/* fun dialogFlow(query: String) {
-     if (query.isNotEmpty()) {
-
-         val accessToken = Helper.getConfigValue(baseActivity, "dialogflow_conn")
-
-         val client = OkHttpClient.Builder()
-             .addInterceptor(okhttpInterceptor())
-             .build()
-
-         val request = okhttp3.Request.Builder()
-             .url("https://api.dialogflow.com/v1/query?v=20150910&lang=en&query=$query&sessionId=12345")
-             .header("Authorization", "Bearer $accessToken")
-             .build()
-
-         Thread {
-             val response = client.newCall(request).execute()
-             val body = response.body().string()
-             response.body().close()
-
-             val jsonBody: JSONObject = JSONObject(body)
-             val gson = GsonBuilder().setLenient().create()
-             //gson.fromJson(body, DFResponseObject::class.java)
-
-             val result: JSONObject = jsonBody["result"] as JSONObject
-             val fulfillment: JSONObject = result["fulfillment"] as JSONObject
-
-             if (fulfillment.has("messages")) {
-                 val messages: JSONArray = fulfillment["messages"] as JSONArray
-
-                 if (messages.length() > 0) {
-                     val message: JSONObject = messages.get(0) as JSONObject
-
-                     if (message.has("speech")) {
-                         baseActivity.runOnUiThread { updateText(message["speech"].toString()) }
-                         baseActivity.muteSound(false)
-                         Speech.speakOutText(message["speech"].toString())
-                         Log.e("-->>>", "$message")
-
-                     }
-                 }
-             } else {
-                 //if (result.has("score") && (result["score"] as Double) < 0.1){
-                 baseActivity.runOnUiThread { updateText("Sorry, I didn't get that.") }
-                 baseActivity.muteSound(false)
-                 Speech.speakOutText("Sorry, I didn't get that.")
-             }
-
-             Log.i("---->", response.toString())
-
-         }.start()
-
-
-     }
-
- }*/
 
 
 
--- app/src/main/java/ai/jetbrain/arya/fragment/WelcomeFragment.kt
+++ app/src/main/java/ai/jetbrain/arya/fragment/WelcomeFragment.kt
@@ -10,24 +10,52 @@ import ai.jetbrain.arya.R
 import ai.jetbrain.arya.SttUtils.Speech
 import ai.jetbrain.arya.activity.BaseActivity
 import ai.jetbrain.arya.utils.CheckNetwork
-import android.annotation.SuppressLint
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.fragment_welcome_screen.*
 
 
 class WelcomeFragment : BaseFragment(), AIListener {
 
     private var aiService: AIService? = null
-    val fragment = SettingsFragment()
-    var busy = false
+    private val fragment = SettingsFragment()
     private var fragmentIsVisible = true
-    var handler = Handler()
+    private var handler = Handler()
     private var runnable: Runnable? = null
+    private val REQUEST_AUDIO_PERMISSION_REQ_ID = 1
 
+    private var isBusy = false
+    private  var isListening = false
+
+    private val defaultText = "Hello, I'm ARYA"
+    private val defaultErrorText = "Sorry. I'm having some trouble."
+
+    private fun initSTT(activity: Activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            var check = ContextCompat.checkSelfPermission(activity, Manifest.permission.RECORD_AUDIO)
+            if (check == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.RECORD_AUDIO))
+                    Toast.makeText(activity, "App required access to audio", Toast.LENGTH_SHORT).show();
+                ActivityCompat.requestPermissions(
+                    activity,
+                    arrayOf(Manifest.permission.RECORD_AUDIO),
+                    REQUEST_AUDIO_PERMISSION_REQ_ID
+                );
+            }
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_welcome_screen, container, false)
@@ -36,83 +64,86 @@ class WelcomeFragment : BaseFragment(), AIListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         baseActivity = activity as BaseActivity
-/*
-
-        if (fragmentIsVisible){
-            handler.postDelayed({
-                checkInternet()
-            }, 5000)
-        }else{
-            mVoiceResult.text = null
-            handler.removeCallbacksAndMessages(null)
+        initSTT(baseActivity)
+        setSpeechAIConfig()
+        mRelativeLayout.setOnClickListener { listen() }
+        mSettings.setOnClickListener { showSettingsFragment() }
+        Speech.setOnCompleteListener {
+            isBusy = false
+            watchForResetText()
         }
-*/
-
-        aiConfig()
-        mRelativeLayout.setOnClickListener { aiListening() }
-        mSettings.setOnClickListener { settingFragment() }
-
     }
+
+    private fun watchForResetText() {
+        baseActivity.runOnUiThread {
+            Handler().postDelayed({
+                if (!isBusy) textUpdate(defaultText)
+            }, 5000)
+        }
+    }
+
     override fun setUserVisibleHint(isVisibleToUser: Boolean) {
         super.setUserVisibleHint(isVisibleToUser)
         fragmentIsVisible = isVisibleToUser
-
+        /*
         if (fragmentIsVisible)
-            batteryHandler()
+            internetHandler()
         else
             handler.removeCallbacksAndMessages(null)
-
+        */
     }
 
     private fun textUpdate(txt: String) {
-        if (fragmentIsVisible)
-            mVoiceResult.text = txt
-        else
-            fragmentIsVisible = true
+        mVoiceResult.text = txt
     }
 
-    private fun settingFragment() {
-        fragmentManager!!.beginTransaction().replace(R.id.mFrameContainer, SettingsFragment())
+    private fun showSettingsFragment() {
+        fragmentManager!!.beginTransaction().replace(R.id.mFrameContainer, fragment)
             .addToBackStack(null).commit()
     }
 
     override fun onFaceUpdate(facex: Float, facey: Float) {
         super.onFaceUpdate(facex, facey)
-        aiListening()
+        if (facex > 0 && facex < 1 && facey > 0 && facey < 1) {
+            if(CheckNetwork.isInternetAvailable(baseActivity)) {
+                listen()
+            }
+            else {
+                textUpdate("I'm having trouble connecting to internet.")
+            }
+        }
     }
 
-    override fun updateText(txt: String) {
-        super.updateText(txt)
+    private fun listen() {
+        if (!isBusy) {
+            isBusy = true
+            isListening = true
+            aiService!!.startListening()
+            Handler().postDelayed({
+                if(isListening) aiService!!.cancel();
+            },10000)
+            textUpdate("Listening...")
+        }
     }
 
-    private fun aiListening() {
-        if (!busy)
-            busy = true
-        aiService!!.startListening()
-
-    }
-
-    private fun aiConfig() {
+    private fun setSpeechAIConfig() {
         val config = ai.api.android.AIConfiguration(
             "f3bec00f4fac4c8fa9e606c2f97a73ef",
             AIConfiguration.SupportedLanguages.English,
             ai.api.android.AIConfiguration.RecognitionEngine.System
         )
-
         aiService = AIService.getService(baseActivity, config)
         aiService!!.setListener(this)
     }
 
     override fun onResult(response: AIResponse) {
         getResultInText(response)
-
     }
 
     override fun onError(error: AIError) {
-        if (CheckNetwork.isInternetAvailable(baseActivity))
-            textUpdate("No speech found")
-        else
-            textUpdate("No Internet")
+        textUpdate(defaultErrorText)
+        isBusy = false
+        watchForResetText()
     }
 
     override fun onAudioLevel(level: Float) {
@@ -121,60 +152,27 @@ class WelcomeFragment : BaseFragment(), AIListener {
 
     override fun onListeningStarted() {
         baseActivity.muteSound(true)
-        if (CheckNetwork.isInternetAvailable(baseActivity))
-            textUpdate("Listening...")
-        else
-            textUpdate("No Internet")
+        listen()
     }
 
     override fun onListeningCanceled() {
-        textUpdate("Hello I'm ARYA")
-
+        isBusy = false
+        isListening = false
+        textUpdate(defaultText)
     }
 
     override fun onListeningFinished() {
         baseActivity.muteSound(true)
+        isListening = false
     }
 
-    @SuppressLint("SetTextI18n")
     private fun getResultInText(response: AIResponse) {
         baseActivity.muteSound(false)
         val resultResponse = response.result
-        val parameterString = StringBuilder()
-        if (resultResponse.parameters != null && !resultResponse.parameters.isEmpty()) {
-            for ((key, value) in resultResponse.parameters) {
-                parameterString.append("(").append(key).append(", ")
-                    .append(value).append(") ")
-            }
-        }
-        //mVoiceResult.text = result1.resolvedQuery
         val result = resultResponse.fulfillment.speech
         mVoiceResult.text = result
-        Speech.setOnCompleteListener {
-            busy = false
-        }
         Speech.speakOutText(result)
-
-    }
-
-    private fun checkInternet() {
-        if (fragmentIsVisible) {
-            if (CheckNetwork.isInternetAvailable(baseActivity)) {
-                aiConfig()
-            } else
-                textUpdate("No Internet")
-        }
-    }
-
-    private fun batteryHandler() {
-        runnable = object : Runnable {
-            override fun run() {
-                checkInternet()
-                handler.postDelayed(this, 5000)
-            }
-        }
-//Start
-        handler.postDelayed(runnable, 3000)
+        textUpdate(result)
     }
 
     override fun onDetach() {
