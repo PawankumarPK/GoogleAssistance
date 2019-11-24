c2024d2dd47df6083fc865cafed41fb55ab27382
PawankumarPK <pk9910765616@gmail.com>
2019-11-24 21:42:53 +0530

    Format code

--- app/src/main/java/ai/jetbrain/arya/static/FaceData.kt
+++ app/src/main/java/ai/jetbrain/arya/static/FaceData.kt
@@ -1,8 +1,6 @@
 package ai.jetbrain.arya.static
-/**
- * Created by ajayvishnu on 27/06/19.
- */
+
 object FaceData {
-    var x:Float = -1F
-    var y:Float = -1F
+    var x: Float = -1F
+    var y: Float = -1F
 }
